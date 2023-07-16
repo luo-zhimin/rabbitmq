c14f3af5e5bb26b115c33d340a48610c75096e17
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-07-16 22:27:25 +0800

    priority queue

--- src/main/java/com/image/rabbitmq/producer/Producer.java
+++ src/main/java/com/image/rabbitmq/producer/Producer.java
@@ -1,7 +1,11 @@
 package com.image.rabbitmq.producer;
 
 import com.image.rabbitmq.util.RabbitMqUtil;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,15 +31,33 @@ public class Producer {
         //boolean exclusive 该队列是否只供一个消费者消费 是否进行消息的共享 true 多个消费者消费 false 单个
         //boolean autoDelete 是否自动删除 最后一个消费者断开连接后 该队列是否自动删除 true delete false no delete
         //Map<String, Object> arguments 其他参数
-        channel.queueDeclare(queue_name, true, false, false, null);
+
+        Map<String, Object> arguments = new HashMap<>(1);
+        //最大优先级 官方容许最大0-255 不要设置太大 浪费cpu和内存
+        arguments.put("x-max-priority", 10);
+
+        channel.queueDeclare(queue_name, true, false, false, arguments);
         //发消息 初次使用
-        String message = "hello word";
+//        String message = "hello word";
         //basicPublish
         //String exchange 发送到哪个交换机
         //String routingKey 路由key值 本次是队列名称
         //BasicProperties props 其他参数
         //byte[] body 发送的消息
-        channel.basicPublish("", queue_name, null, message.getBytes());
+        for (int i = 0; i < 10; i++) {
+            String message = "info" + i;
+            if (i == 5) {
+                //优先级高一些
+                AMQP.BasicProperties basicProperties = new AMQP.BasicProperties().builder()
+                        .priority(5)
+                        .build();
+//                basicProperties.builder().priority(5).build();
+                channel.basicPublish("", queue_name, basicProperties, message.getBytes());
+                ;
+            } else {
+                channel.basicPublish("", queue_name, null, message.getBytes());
+            }
+        }
         System.out.println("消息发送完毕");
     }
 }
